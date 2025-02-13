4e0e72adb0da3c552b81017de57c6e39135f5e2b
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2025-02-13 17:35:19 +1300

    Documentation.

--- lib/fiber/profiler.rb
+++ lib/fiber/profiler.rb
@@ -7,10 +7,18 @@ require_relative "profiler/version"
 require_relative "profiler/native"
 
 module Fiber::Profiler
+	# The default profiler to use, if any.
+	#
+	# Use the `FIBER_PROFILER_CAPTURE=true` environment variable to enable profiling.
+	#
+	# @returns [Capture | Nil]
 	def self.default
 		Capture.default
 	end
 	
+	# Execute the given block with the {default} profiler, if any.
+	#
+	# @yields {...} The block to execute.
 	def self.capture
 		if capture = self.default
 			begin
